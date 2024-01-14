5840431e4557b25e327d0153c73e5feac8705293
mailyovas@gmail.com <mailyovas@gmail.com>
2024-01-14 18:11:36 +0530

    Third Commit for checking Jenkins

---
+++ src/main/java/POM/Dropdown.java
@@ -0,0 +1,21 @@
+package POM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Dropdown {
+	@FindBy(xpath= "//form/input[@id='text']")
+	WebElement LoginId;
+	@FindBy(xpath= "//form/input[@id='password']")
+	WebElement Password;
+	@FindBy(xpath= "//form/button[@id='login-button']")
+	WebElement LoginBtn;
+	private WebDriver driver;
+	public Dropdown(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+}
